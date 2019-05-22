119a0228ab8feec082e8033574b600c490c55a38
Kartik Shenoy <kartik.shenoyy@gmail.com>
2019-05-23 00:28:10 +0530

    Realtime voting app implemented

--- Server/index.js
+++ Server/index.js
@@ -32,3 +32,37 @@ app.use('/',router);
 http.listen(3000,function(){
     console.log("Listening on 3000");
 });
+
+
+var noOfUsers = 0;
+
+io.on('connection',function(socket){
+    // This function will be called when a new connection is established
+    
+    console.log('New user connected !');
+    noOfUsers++;
+
+    console.log("Total "+noOfUsers+" users are connected");
+
+    // This event will be emitted from Client when some one add comments.
+    socket.on('voted',function(data){
+        console.log("Received Vote",data)
+        polls[data.vote]++;
+        console.log(polls);
+
+        io.emit('notify0everyone',polls); // for all.
+        // socket.broadcast.emit("notify0everyone",polls); // for all except me.
+
+
+        
+        // Add the comment in database.
+        // db.addComment(data.user,data.comment,mysql,pool,function(error,result){
+        //     if (error) {
+        //         io.emit('error');
+        //     } else {
+        //         // On successful addition, emit event for client.
+        //         socket.broadcast.emit("notify everyone",{user : data.user,comment : data.comment});
+        //     }
+        // });
+    });
+});
